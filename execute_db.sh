40daca0173440a9d6b36b93df13c263ef7e9d00c
shafi Vidpanakal <77560604+Merema001@users.noreply.github.com>
2021-02-08 06:40:31 +0530

    Update execute_db.sh

--- execute_db.sh
+++ execute_db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 yum install mysql -y
-#export MYSQL_PWD=password
-a=$(curl http://169.254.169.254/laglobal/meta-data/instance-id)
-b=$(curl http://169.254.169.254/laglobal/meta-data/local-ipv4)
+export MYSQL_PWD=global123
+a=$(cat /var/lib/cloud/data/instance-id)
+b=$(ifconfig|awk -v RS='' '/eth0/'|sed -n 2p|awk '{print $2}')
 c="$(date '+%Y:%m:%d %H:%M:%S')"
 echo $c
 aws s3 cp s3://s3-terraform-bucket-global/profile /tmp/insert.sql
@@ -10,4 +10,4 @@ chmod 755 /tmp/insert.sql
 sed -i 's/$a/'$a'/g' /tmp/insert.sql
 sed -i 's/$b/'$b'/g' /tmp/insert.sql
 sed -i 's/$c/'"$c"'/g' /tmp/insert.sql
-mysql -u global -pglobal123 -h globalscale1.cfpuoxqzhp3k.us-east-1.rds.amazonaws.com  -P 3306 < /tmp/insert.sql
+mysql -u global -p$MYSQL_PWD -h globalscale1.cfpuoxqzhp3k.us-east-1.rds.amazonaws.com  -P 3306 < /tmp/insert.sql
